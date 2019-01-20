156c5861447d04e092e42a3e77060410ecd1a3b2
bogusz2233 <bogusz2233@gmail.com>
2019-01-20 22:21:20 +0100

    adding view for chart to presents hour temperature adding content  conteiner

--- weather/weather.js
+++ weather/weather.js
@@ -141,3 +141,35 @@ var keyPressedText = (event) => {
 var converFahrToCels = (argTempConver) =>{
     return ( (argTempConver - 32) * 5 / 9 ).toFixed(1);
 }
+
+var printWeatherChartTest = () => {
+    var charContex = document.getElementById("weatherChart").getContext('2d');
+    var myChart = new Chart(charContex, {
+        type: 'line',
+        data: {
+            labels: [11231231132155515, 11231231132155513, 112312311321555133, 112312311321555134, 5, 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40],
+            datasets: [{
+                label: 'Temperatura',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+            
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+}
