866261c7f7414340b6a0bc821daabd92ccde00cc
bogusz2233 <bogusz2233@gmail.com>
2019-01-24 17:03:20 +0100

    add new scroll feature

--- script.js
+++ script.js
@@ -11,17 +11,18 @@ window.onload = () => {
         var container = document.getElementById("contentContainer");
         container.style.marginTop = "50px";
     }
+
 };
 
 // function to create all window and querry that from data base
-var createContentContainerView = () =>{
+var createContentContainerView = () => {
 
-    getConentCount().then((resultCount) =>{
+    getConentCount().then((resultCount) => {
         //console.log(resultCount);
         return loadAllContentWindow(resultCount);
-    }).then( (result) =>{
+    }).then((result) => {
 
-    }).catch( (errorMes) =>{
+    }).catch((errorMes) => {
         console.log(errorMes);
     });
 };
@@ -68,19 +69,17 @@ var addNewPost = (argTitle, argOverloopText, argImgSrc, argImgBackround, argText
 
     //img
     console.log(argImgSrc);
-    if(argImgSrc === undefined)
-    {
+    if (argImgSrc === undefined) {
         console.log(`${SERVER_CONTETN_URL}/noimage.jpg`);
         dataBlockImg.src = `${SERVER_CONTETN_URL}/noimage.jpg`;
         dataBlockImg.style.background = "#4f5051";
     }
-    else
-    {
+    else {
         dataBlockImg.src = `${SERVER_URL}/${argImgSrc}`;
         dataBlockImg.style.background = argImgBackround;
     }
     dataBlockImg.onerror = function () { onImageError(dataBlockImg) };
-    
+
 
     //desc 
     if (argTextDesc != undefined) dataBlockDesc.textContent = argTextDesc;
@@ -91,13 +90,13 @@ var addNewPost = (argTitle, argOverloopText, argImgSrc, argImgBackround, argText
     dataBlockDesc.style.whiteSpace = "pre-wrap";
 
     //dodanie linku:
-    dataBlock.onclick = () => { 
-        if(argLinkToPost != undefined)
-        location.href = argLinkToPost; 
+    dataBlock.onclick = () => {
+        if (argLinkToPost != undefined)
+            location.href = argLinkToPost;
     };
 
     // dodanie stworzonego bloku do kontenera
-    document.getElementById("contentContainer").prepend(dataBlock); 
+    document.getElementById("contentContainer").prepend(dataBlock);
 };
 
 // funkcja wywowyłana podczas błedu
@@ -139,31 +138,64 @@ var getConentCount = () => {
     });
 };
 
+var addDataBlockAnimation = () => {
+    ScrollReveal().reveal('.dataBlock:nth-child(3n-2)', {
+        delay: 300,
+        duration: 2500,
+        viewFactor: 0.5,
+        reset: true,
+        distance: '1000px',
+        origin: 'left',
+        easing: 'cubic-bezier(0.5, 0, 0, 1)',
+        opacity: 0
+    });
+    ScrollReveal().reveal('.dataBlock:nth-child(3n -1)', {
+        delay: 300,
+        duration: 2500,
+        viewFactor: 0.5,
+        reset: true,
+        distance: '1000px',
+        origin: 'rigth',
+        easing: 'cubic-bezier(0.5, 0, 0, 1)',
+        opacity: 0
+    });
+
+    ScrollReveal().reveal('.dataBlock:nth-child(3n)', {
+        delay: 300,
+        duration: 2500,
+        viewFactor: 0.5,
+        reset: true,
+        distance: '500px',
+        origin: 'bottom',
+        easing: 'cubic-bezier(0.5, 0, 0, 1)',
+        opacity: 0
+    });
+}
 //function to load one window from base
-var createContet = (argWindowNO, argWindowCount,resolve,reject) =>{
+var createContet = (argWindowNO, argWindowCount, resolve, reject) => {
     let xmlhttp = new XMLHttpRequest();
     let url = `${SERVER_CONTETN_URL}?${CONTENT_NUM_QUERY}=${argWindowNO}`;
     xmlhttp.open("GET", url, true);
     xmlhttp.onload = () => {
         let jsonResponse = JSON.parse(xmlhttp.responseText);
         addNewPost(jsonResponse.body.title, jsonResponse.body.secondTitle, jsonResponse.body.imgScr, jsonResponse.body.bckImgColor, jsonResponse.body.desc, jsonResponse.body.link);
-        if(argWindowNO == argWindowCount -1)
-        {
+        if (argWindowNO == argWindowCount - 1) {
+
+            addDataBlockAnimation();
             resolve(console.log("ALL loaded!"));
         }
-        else
-        {
-            createContet(argWindowNO + 1,argWindowCount,resolve,reject);
+        else {
+            createContet(argWindowNO + 1, argWindowCount, resolve, reject);
         }
-        
+
     };
     xmlhttp.onerror = () => reject(xmlhttp.statusText);
     xmlhttp.send();
 };
 
 //function to load all posts from base
-var loadAllContentWindow = (argWindowCount) =>{
+var loadAllContentWindow = (argWindowCount) => {
     return new Promise((resolve, reject) => {
-        createContet(0,argWindowCount, resolve, reject);
+        createContet(0, argWindowCount, resolve, reject);
     });
 };
